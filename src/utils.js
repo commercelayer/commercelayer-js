d2626104e381bcd62b63fbfc44dc2d18254bd7db
Filippo Conforti <filippo@commercelayer.io>
2019-01-03 17:45:58 +0100

    Version 1.3.0

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ const cookies = require('js-cookie')
 
 module.exports = {
   getOrderCookieName: function() {
-    return `order_token_${config.countryCode()}`
+    return `order_token_${config.clientId()}_${config.marketId()}_${config.countryCode()}`
   },
   getAccessTokenCookieName: function() {
     return `access_token_${config.clientId()}_${config.marketId()}`
