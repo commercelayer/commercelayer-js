1abb88d12cb1371a60e5b543478059e36ecfd328
Filippo Conforti <filippo@commercelayer.io>
2019-01-03 14:29:10 +0100

    Version 1.2.9

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,17 @@
-const modernizr = require("modernizr")
 const axios = require('axios')
 const config = require('./config')
 const utils = require('./utils')
+
+function checkCookie(){
+  // Quick test if browser has cookieEnabled host property
+  if (navigator.cookieEnabled) return true;
+  // Create cookie
+  document.cookie = "cookietest=1";
+  var ret = document.cookie.indexOf("cookietest=") != -1;
+  // Delete cookie
+  document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+  return ret;
+}
 
 function getAccessToken() {
   return axios
@@ -32,7 +42,7 @@ axios.interceptors.response.use(function (response) {
   return response
 }, function (error) {
   if (error.response.status === 401) {
-    if (modernizr.cookies) {
+    if (checkCookie()) {
       if (utils.getAccessTokenRetryLockCookie() == undefined) {
         utils.setAccessTokenRetryLockCookie()
         return getAccessToken().then(function(accessToken) {
--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,5 @@ module.exports = {
   output: {
     filename: 'commercelayer.min.js',
     path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        loader: 'webpack-modernizr-loader',
-        options: {
-          "feature-detects": [
-            "cookies"
-          ]
-        },
-        test: /empty-alias-file\.js$/
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      modernizr$: path.resolve(__dirname, "./empty-alias-file.js")
-    }
   }
 }
