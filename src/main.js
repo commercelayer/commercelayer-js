b0147328049acb268a128f594b27944f5a2a8a6b
Filippo Conforti <filippo@commercelayer.io>
2019-01-04 11:54:14 +0100

    Version 1.3.9

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const api = require('./api')
-  const listeners = require('./listeners')
-  const utils = require('./utils')
+document.addEventListener('readystatechange', function() {
+  if (document.readyState == 'interactive') {
+    const api = require('./api')
+    const listeners = require('./listeners')
+    const utils = require('./utils')
 
-  exports.init = function() {
-    api.getPrices()
-    api.getVariants()
-    listeners.setVariantSelect()
-    listeners.setAddToShoppingBag()
-    listeners.setShoppingBagToggle()
-    api.refreshOrder()
+    exports.init = function() {
+      api.getPrices()
+      api.getVariants()
+      listeners.setVariantSelect()
+      listeners.setAddToShoppingBag()
+      listeners.setShoppingBagToggle()
+      api.refreshOrder()
+    }
+
+    document.commercelayer = module.exports    
+    document.commercelayer.init()
   }
-
-  window.commercelayer = module.exports
-  module.exports.init()
 })
