6821c7066a04e721a52c4b9edd53b2ada58689c2
Alessandro Casazza <imasterale@gmail.com>
2019-09-05 19:25:31 +0200

    Added new feature clayer-add-variant-quantity

--- src/listeners.js
+++ src/listeners.js
@@ -3,58 +3,117 @@ const api = require('./api')
 const utils = require('./utils')
 
 module.exports = {
-
   setupVariants: function() {
     let variantSelects = document.querySelectorAll('.clayer-variant-select')
-    variantSelects.forEach(function (variantSelect) {
-      variantSelect.addEventListener('change', function(event){
+    variantSelects.forEach(function(variantSelect) {
+      variantSelect.addEventListener('change', function(event) {
         let selectedOption = variantSelect.options[this.selectedIndex]
-        api.selectSku(selectedOption.value, selectedOption.dataset.skuName, selectedOption.dataset.skuReference, selectedOption.dataset.skuImageUrl, this.dataset.priceContainerId, this.dataset.availabilityMessageContainerId, this.dataset.addToBagId)
+        api.selectSku(
+          selectedOption.value,
+          selectedOption.dataset.skuName,
+          selectedOption.dataset.skuReference,
+          selectedOption.dataset.skuImageUrl,
+          this.dataset.priceContainerId,
+          this.dataset.availabilityMessageContainerId,
+          this.dataset.addToBagId,
+          this.dataset.addVariantQuantityId
+        )
       })
     })
 
     let variantRadios = document.querySelectorAll('.clayer-variant-radio')
-    variantRadios.forEach(function (variantRadio) {
-      variantRadio.addEventListener('click', function(event){
-        api.selectSku(this.value, this.dataset.skuName, this.dataset.skuReference, this.dataset.skuImageUrl, this.dataset.priceContainerId, this.dataset.availabilityMessageContainerId, this.dataset.addToBagId)
+    variantRadios.forEach(function(variantRadio) {
+      variantRadio.addEventListener('click', function(event) {
+        api.selectSku(
+          this.value,
+          this.dataset.skuName,
+          this.dataset.skuReference,
+          this.dataset.skuImageUrl,
+          this.dataset.priceContainerId,
+          this.dataset.availabilityMessageContainerId,
+          this.dataset.addToBagId,
+          this.dataset.addVariantQuantityId
+        )
       })
     })
   },
-
+  setupAddVariantQuantity: () => {
+    const addVariantsQuantity = document.querySelectorAll(
+      '.clayer-add-variant-quantity'
+    )
+    addVariantsQuantity.forEach(addVariantQuantity => {
+      addVariantQuantity.addEventListener('change', event => {
+        event.preventDefault()
+        const min = addVariantQuantity.max !== '' && Number(addVariantQuantity.min)
+        const max = addVariantQuantity.max !== '' && Number(addVariantQuantity.max)
+        const val = Number(addVariantQuantity.value)
+        if (max && val > max) {
+          addVariantQuantity.value = max
+        } else if (min && val < min) {
+          addVariantQuantity.value = min
+        }
+      })
+    })
+  },
   setupAddToBags: function() {
-    let addToBags = document.querySelectorAll(".clayer-add-to-bag")
+    let addToBags = document.querySelectorAll('.clayer-add-to-bag')
 
-    addToBags.forEach(function (addToBag) {
+    addToBags.forEach(function(addToBag) {
       addToBag.addEventListener('click', function(event) {
         event.preventDefault()
+        let quantity = 1
+        const variantQuantity = document.querySelector(
+          `#${addToBag.dataset.addVariantQuantityId}`
+          )
+        if (variantQuantity) {
+          const val = Number(variantQuantity.value)
+          const quantityMax = variantQuantity.max !== '' && Number(variantQuantity.max)
+          if (quantityMax && val > quantityMax) {
+            return false
+          }
+          quantity = variantQuantity.value
+        }
+        let orderPromise = utils.getOrderToken()
+          ? api.getOrder()
+          : api.createOrder()
 
-        let orderPromise = utils.getOrderToken() ? api.getOrder() : api.createOrder()
-
-        orderPromise.then(function(order){
-
-          api.createLineItem(order.id, addToBag.dataset.skuId, addToBag.dataset.skuName, addToBag.dataset.skuReference, addToBag.dataset.skuImageUrl).then(function(lineItem){
-            api.getOrder()
-            ui.openShoppingBag()
-          })
-          .catch(function(error) {
-            switch(error.status) {
-              case 422:
-                let availabilityMessageContainer = document.querySelector(`#${addToBag.dataset.availabilityMessageContainerId}`)
-                if (availabilityMessageContainer) {
-                  ui.displayUnavailableMessage(availabilityMessageContainer)
-                }
-                break
-            }
-          })
+        orderPromise.then(function(order) {
+          api
+            .createLineItem(
+              order.id,
+              addToBag.dataset.skuId,
+              addToBag.dataset.skuName,
+              addToBag.dataset.skuReference,
+              addToBag.dataset.skuImageUrl,
+              quantity
+            )
+            .then(function(lineItem) {
+              api.getOrder()
+              ui.openShoppingBag()
+            })
+            .catch(function(error) {
+              switch (error.status) {
+                case 422:
+                  let availabilityMessageContainer = document.querySelector(
+                    `#${addToBag.dataset.availabilityMessageContainerId}`
+                  )
+                  if (availabilityMessageContainer) {
+                    ui.displayUnavailableMessage(availabilityMessageContainer)
+                  }
+                  break
+              }
+            })
         })
       })
     })
   },
 
   setupShoppingBagToggles: function() {
-    let shoppingBagToggles = document.querySelectorAll('.clayer-shopping-bag-toggle')
-    shoppingBagToggles.forEach(function(shoppingBagToggle){
-      shoppingBagToggle.addEventListener('click', function(event){
+    let shoppingBagToggles = document.querySelectorAll(
+      '.clayer-shopping-bag-toggle'
+    )
+    shoppingBagToggles.forEach(function(shoppingBagToggle) {
+      shoppingBagToggle.addEventListener('click', function(event) {
         event.preventDefault()
         ui.toggleShoppingBag()
       })
--- src/ui.js
+++ src/ui.js
@@ -3,15 +3,15 @@ const normalize = require('json-api-normalize')
 
 function enableElement(element) {
   if (element) {
-    element.removeAttribute("disabled")
-    element.classList.remove("disabled")
+    element.removeAttribute('disabled')
+    element.classList.remove('disabled')
   }
 }
 
 function disableElement(element) {
   if (element) {
-    element.setAttribute("disabled", "disabled")
-    element.classList.add("disabled")
+    element.setAttribute('disabled', 'disabled')
+    element.classList.add('disabled')
   }
 }
 
@@ -35,50 +35,64 @@ function setElementHTML(parent, selector, html) {
 }
 
 function updateShoppingBagItemsCount(order) {
-  let shoppingBagItemsCounts = document.querySelectorAll('.clayer-shopping-bag-items-count')
-  shoppingBagItemsCounts.forEach(function(shoppingBagItemsCount){
+  let shoppingBagItemsCounts = document.querySelectorAll(
+    '.clayer-shopping-bag-items-count'
+  )
+  shoppingBagItemsCounts.forEach(function(shoppingBagItemsCount) {
     shoppingBagItemsCount.innerHTML = order.attributes.skus_count
   })
 }
 
 function updateShoppingBagTotal(order) {
-  let shoppingBagTotals = document.querySelectorAll('.clayer-shopping-bag-total')
-  shoppingBagTotals.forEach(function(shoppingBagTotal){
-    shoppingBagTotal.innerHTML = order.attributes.formatted_total_amount_with_taxes
+  let shoppingBagTotals = document.querySelectorAll(
+    '.clayer-shopping-bag-total'
+  )
+  shoppingBagTotals.forEach(function(shoppingBagTotal) {
+    shoppingBagTotal.innerHTML =
+      order.attributes.formatted_total_amount_with_taxes
   })
 }
 
 function updateShoppingBagSubtotal(order) {
-  let shoppingBagSubtotals = document.querySelectorAll('.clayer-shopping-bag-subtotal')
-  shoppingBagSubtotals.forEach(function(shoppingBagSubtotal){
+  let shoppingBagSubtotals = document.querySelectorAll(
+    '.clayer-shopping-bag-subtotal'
+  )
+  shoppingBagSubtotals.forEach(function(shoppingBagSubtotal) {
     shoppingBagSubtotal.innerHTML = order.attributes.formatted_subtotal_amount
   })
 }
 
 function updateShoppingBagShipping(order) {
-  let shoppingBagShippings = document.querySelectorAll('.clayer-shopping-bag-shipping')
-  shoppingBagShippings.forEach(function(shoppingBagShipping){
+  let shoppingBagShippings = document.querySelectorAll(
+    '.clayer-shopping-bag-shipping'
+  )
+  shoppingBagShippings.forEach(function(shoppingBagShipping) {
     shoppingBagShipping.innerHTML = order.attributes.formatted_shipping_amount
   })
 }
 
 function updateShoppingBagPayment(order) {
-  let shoppingBagPayments = document.querySelectorAll('.clayer-shopping-bag-payment')
-  shoppingBagPayments.forEach(function(shoppingBagPayment){
-    shoppingBagPayment.innerHTML = order.attributes.formatted_payment_method_amount
+  let shoppingBagPayments = document.querySelectorAll(
+    '.clayer-shopping-bag-payment'
+  )
+  shoppingBagPayments.forEach(function(shoppingBagPayment) {
+    shoppingBagPayment.innerHTML =
+      order.attributes.formatted_payment_method_amount
   })
 }
 
 function updateShoppingBagTaxes(order) {
   let shoppingBagTaxes = document.querySelectorAll('.clayer-shopping-bag-taxes')
-  shoppingBagTaxes.forEach(function(shoppingBagTax){
+  shoppingBagTaxes.forEach(function(shoppingBagTax) {
     shoppingBagTax.innerHTML = order.attributes.formatted_total_tax_amount
   })
 }
 
 function updateShoppingBagDiscount(order) {
-  let shoppingBagDiscounts = document.querySelectorAll('.clayer-shopping-bag-discount')
-  shoppingBagDiscounts.forEach(function(shoppingBagDiscount){
+  let shoppingBagDiscounts = document.querySelectorAll(
+    '.clayer-shopping-bag-discount'
+  )
+  shoppingBagDiscounts.forEach(function(shoppingBagDiscount) {
     shoppingBagDiscount.innerHTML = order.attributes.formatted_discount_amount
   })
 }
@@ -93,22 +107,32 @@ module.exports = {
       }
       priceCompareAmount = priceContainer.querySelector('.compare-at-amount')
       if (priceCompareAmount) {
-        if (sku.prices[0].compare_at_amount_cents > sku.prices[0].amount_cents) {
-          priceCompareAmount.innerHTML = sku.prices[0].formatted_compare_at_amount
+        if (
+          sku.prices[0].compare_at_amount_cents > sku.prices[0].amount_cents
+        ) {
+          priceCompareAmount.innerHTML =
+            sku.prices[0].formatted_compare_at_amount
         }
       }
     }
   },
   updatePrices: function(skus) {
-    skus.forEach( function(sku){
-      priceAmounts = document.querySelectorAll('[data-sku-code="' + sku.code + '"] > .amount')
-      priceAmounts.forEach(function (priceAmount) {
+    skus.forEach(function(sku) {
+      priceAmounts = document.querySelectorAll(
+        '[data-sku-code="' + sku.code + '"] > .amount'
+      )
+      priceAmounts.forEach(function(priceAmount) {
         priceAmount.innerHTML = sku.prices[0].formatted_amount
       })
-      priceCompareAmounts = document.querySelectorAll('[data-sku-code="' + sku.code + '"] > .compare-at-amount')
-      priceCompareAmounts.forEach(function (priceCompareAmount) {
-        if (sku.prices[0].compare_at_amount_cents > sku.prices[0].amount_cents) {
-          priceCompareAmount.innerHTML = sku.prices[0].formatted_compare_at_amount
+      priceCompareAmounts = document.querySelectorAll(
+        '[data-sku-code="' + sku.code + '"] > .compare-at-amount'
+      )
+      priceCompareAmounts.forEach(function(priceCompareAmount) {
+        if (
+          sku.prices[0].compare_at_amount_cents > sku.prices[0].amount_cents
+        ) {
+          priceCompareAmount.innerHTML =
+            sku.prices[0].formatted_compare_at_amount
         }
       })
     })
@@ -116,38 +140,101 @@ module.exports = {
   updateVariants: function(skus, clear) {
     if (clear === true) {
       let allVariants = document.querySelectorAll('.clayer-variant')
-      allVariants.forEach(function (variant) {
+      allVariants.forEach(function(variant) {
         disableElement(variant)
       })
     }
-    skus.forEach( function(sku){
-      let variants = document.querySelectorAll('.clayer-variant[data-sku-code="' + sku.code + '"]')
-      variants.forEach(function (variant) {
+    skus.forEach(function(sku) {
+      let variants = document.querySelectorAll(
+        '.clayer-variant[data-sku-code="' + sku.code + '"]'
+      )
+      variants.forEach(function(variant) {
         variant.value = sku.id
         enableElement(variant)
       })
     })
   },
+  updateVariantsQuantity: function(skus) {
+    console.log('skus :', skus)
+    let allAddVariantQuantity = document.querySelectorAll(
+      '.clayer-add-variant-quantity'
+    )
+    allAddVariantQuantity.forEach(function(addVariantQuantity) {
+      disableElement(addVariantQuantity)
+    })
+    skus.forEach(function(sku) {
+      let addVariantsQuantity = document.querySelectorAll(
+        '.clayer-add-variant-quantity[data-sku-code="' + sku.code + '"]'
+      )
+      addVariantsQuantity.forEach(function(addVariantQuantity) {
+        addVariantQuantity.dataset.skuId = sku.id
+        enableElement(addVariantQuantity)
+      })
+    })
+  },
+  updateAddVariantQuantitySKU: function(
+    skuId,
+    skuName,
+    skuReference,
+    skuImageUrl,
+    skuMaxQuantity,
+    addVariantQuantityId
+  ) {
+    let addVariantQuantity = document.querySelector(`#${addVariantQuantityId}`)
+    if (addVariantQuantity) {
+      addVariantQuantity.dataset.skuId = skuId
+      addVariantQuantity.value = 1
+      addVariantQuantity.min = 1
+      if (skuName) addVariantQuantity.dataset.skuName = skuName
+      if (skuReference) addVariantQuantity.dataset.skuReference = skuReference
+      if (skuImageUrl) addVariantQuantity.dataset.skuImageUrl = skuImageUrl
+      if (skuMaxQuantity) addVariantQuantity.max = skuMaxQuantity
+    }
+  },
+
   updateAddToBags: function(skus) {
     let allAddToBags = document.querySelectorAll('.clayer-add-to-bag')
-    allAddToBags.forEach(function (addToBag) {
+    allAddToBags.forEach(function(addToBag) {
       disableElement(addToBag)
     })
-    skus.forEach(function(sku){
-      let addToBags = document.querySelectorAll('.clayer-add-to-bag[data-sku-code="' + sku.code + '"]')
-      addToBags.forEach(function (addToBag){
+    skus.forEach(function(sku) {
+      let addToBags = document.querySelectorAll(
+        '.clayer-add-to-bag[data-sku-code="' + sku.code + '"]'
+      )
+      addToBags.forEach(function(addToBag) {
         addToBag.dataset.skuId = sku.id
         enableElement(addToBag)
       })
     })
   },
-  updateAddToBagSKU: function(skuId,skuName,skuReference,skuImageUrl,addToBagId) {
+  updateAddToBagSKU: function(
+    skuId,
+    skuName,
+    skuReference,
+    skuImageUrl,
+    addToBagId,
+    addVariantQuantityId
+  ) {
     let addToBag = document.querySelector(`#${addToBagId}`)
     if (addToBag) {
       addToBag.dataset.skuId = skuId
       if (skuName) addToBag.dataset.skuName = skuName
       if (skuReference) addToBag.dataset.skuReference = skuReference
       if (skuImageUrl) addToBag.dataset.skuImageUrl = skuImageUrl
+      if (addVariantQuantityId)
+        addToBag.dataset.addVariantQuantityId = addVariantQuantityId
+    }
+  },
+  enableAddVariantQuantity: function(addVariantQuantityId) {
+    let addVariantQuantity = document.querySelector(`#${addVariantQuantityId}`)
+    if (addVariantQuantity) {
+      enableElement(addVariantQuantity)
+    }
+  },
+  disableAddVariantQuantity: function(addVariantQuantityId) {
+    let addVariantQuantity = document.querySelector(`#${addVariantQuantityId}`)
+    if (addVariantQuantity) {
+      disableElement(addVariantQuantity)
     }
   },
   enableAddToBag: function(addToBagId) {
@@ -162,8 +249,13 @@ module.exports = {
       disableElement(addToBag)
     }
   },
-  updateAvailabilityMessage: function(inventory, availabilityMessageContainerId) {
-    let availabilityMessageContainer = document.querySelector(`#${availabilityMessageContainerId}`)
+  updateAvailabilityMessage: function(
+    inventory,
+    availabilityMessageContainerId
+  ) {
+    let availabilityMessageContainer = document.querySelector(
+      `#${availabilityMessageContainerId}`
+    )
     if (availabilityMessageContainer) {
       let first_level = utils.getInventoryFirstAvailableLevel(inventory)
       if (first_level.quantity > 0) {
@@ -183,12 +275,11 @@ module.exports = {
     updateShoppingBagDiscount(order)
   },
   updateShoppingBagCheckout: function(order) {
-    let shoppingBagCheckouts = document.querySelectorAll('.clayer-shopping-bag-checkout')
-    shoppingBagCheckouts.forEach(function(shoppingBagCheckout){
-      normalized_order = order.get([
-        'line_items.id',
-        'checkout_url'
-      ])[0]
+    let shoppingBagCheckouts = document.querySelectorAll(
+      '.clayer-shopping-bag-checkout'
+    )
+    shoppingBagCheckouts.forEach(function(shoppingBagCheckout) {
+      normalized_order = order.get(['line_items.id', 'checkout_url'])[0]
 
       if (normalized_order.line_items) {
         enableElement(shoppingBagCheckout)
@@ -200,7 +291,6 @@ module.exports = {
     })
   },
   displayAvailableMessage: function(container, stockLevel) {
-
     dlt = stockLevel.delivery_lead_times[0]
     qty = stockLevel.quantity
     minDays = dlt.min.days
@@ -211,19 +301,48 @@ module.exports = {
     shippingMethodPrice = dlt.shipping_method.formatted_price_amount
 
     if (container) {
-      let template = document.querySelector('#clayer-availability-message-available-template')
+      let template = document.querySelector(
+        '#clayer-availability-message-available-template'
+      )
 
       if (template) {
-
         let element = utils.getElementFromTemplate(template)
 
-        setElementHTML(element, '.clayer-availability-message-available-qty', qty)
-        setElementHTML(element, '.clayer-availability-message-available-min-days', minDays)
-        setElementHTML(element, '.clayer-availability-message-available-max-days', maxDays)
-        setElementHTML(element, '.clayer-availability-message-available-min-hours', minHours)
-        setElementHTML(element, '.clayer-availability-message-available-max-hours', maxHours)
-        setElementHTML(element, '.clayer-availability-message-available-shipping-method-name', shippingMethodName)
-        setElementHTML(element, '.clayer-availability-message-available-shipping-method-price', shippingMethodPrice)
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-qty',
+          qty
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-min-days',
+          minDays
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-max-days',
+          maxDays
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-min-hours',
+          minHours
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-max-hours',
+          maxHours
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-shipping-method-name',
+          shippingMethodName
+        )
+        setElementHTML(
+          element,
+          '.clayer-availability-message-available-shipping-method-price',
+          shippingMethodPrice
+        )
 
         container.innerHTML = ''
         container.appendChild(element)
@@ -233,7 +352,9 @@ module.exports = {
   },
   displayUnavailableMessage: function(container) {
     if (container) {
-      let template = document.querySelector('#clayer-availability-message-unavailable-template')
+      let template = document.querySelector(
+        '#clayer-availability-message-unavailable-template'
+      )
       if (template) {
         let element = utils.getElementFromTemplate(template)
         container.innerHTML = ''
@@ -243,33 +364,40 @@ module.exports = {
     }
   },
   toggleShoppingBag: function() {
-    let shoppingBagContainer = document.querySelector('#clayer-shopping-bag-container')
+    let shoppingBagContainer = document.querySelector(
+      '#clayer-shopping-bag-container'
+    )
     if (shoppingBagContainer) {
-      shoppingBagContainer.classList.toggle("open")
+      shoppingBagContainer.classList.toggle('open')
     }
     let main = document.querySelector('#clayer-main')
     if (main) {
-      main.classList.toggle("open")
+      main.classList.toggle('open')
     }
   },
   openShoppingBag: function() {
-    let shoppingBagContainer = document.querySelector('#clayer-shopping-bag-container')
+    let shoppingBagContainer = document.querySelector(
+      '#clayer-shopping-bag-container'
+    )
     if (shoppingBagContainer) {
-      shoppingBagContainer.classList.add("open")
+      shoppingBagContainer.classList.add('open')
     }
     let main = document.querySelector('#clayer-main')
     if (main) {
-      main.classList.remove("open")
+      main.classList.remove('open')
     }
   },
   clearShoppingBag: function() {
-    if(document.querySelector('#clayer-shopping-bag-items-container')){
-      document.querySelector('#clayer-shopping-bag-items-container').innerHTML = ''
+    if (document.querySelector('#clayer-shopping-bag-items-container')) {
+      document.querySelector('#clayer-shopping-bag-items-container').innerHTML =
+        ''
     }
   },
   hideAvailabilityMessages: function() {
-    let allAvailabilityMessageContainers = document.querySelectorAll('.clayer-availability-message-container')
-    allAvailabilityMessageContainers.forEach(function(container){
+    let allAvailabilityMessageContainers = document.querySelectorAll(
+      '.clayer-availability-message-container'
+    )
+    allAvailabilityMessageContainers.forEach(function(container) {
       hideElement(container)
     })
   }
